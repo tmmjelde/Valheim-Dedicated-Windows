5777350287e782762ca6378bb794f7356c173258
Thomas Mjelde <thomas@mjelde.it>
2021-03-02 13:40:15 +0100

    Fixes and additions
    Fixed the stop-valheim incorrectly shutting down the main script window.
    Added waiting for the valheim_server PID to terminate before finishing stop-valheim function.
    Added backup functionality
    Updated config file
    A little more output when new version is found.
    If backups is enabled, the backup will run before starting up Valheim after patching.

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 #You need to download SteamCMD and install the server yourself to C:\Valheim.
 #If your paths differ, update the script and config file with your own paths.
 
+#Backups are in testing, and not completely verified yet.
+#Backups will not be made unless BackupsEnabled:True in your config file.
+#BackupsFolder will be created if it doesn't exist.
+#BackupsDaysToKeep will create a lot of data over time if set to 7 days.
+#Consider that Valheim creates an automatic backup every 20 minutes, as well as manual "Save" commands from console, and every time "Stop-Valheim" is used.
+#If your world is 70MB, it will be 7 days * 24 hours * 3 backups per hour * 70 MB = 35GB
+
 #Contents example of the config file:
 
 {
@@ -12,5 +19,9 @@
     "password": "notsosecret",
     "gameid":  "896660",
     "steamcmd":  "C:\\Steamcmd\\steamcmd.exe",
-    "forceinstalldir":  "C:\\Valheim"
+    "forceinstalldir":  "C:\\Valheim",
+    "BackupsEnabled":  "False",
+    "BackupsFolder":  "C:\\ValheimBackup",
+    "BackupsDaysToKeep":  "7"
 }
+
--- Updatecheck.ps1
+++ Updatecheck.ps1
@@ -7,11 +7,15 @@ $Config = Get-Content "C:\SteamCMD\Updatecheck.config" | convertfrom-json
     "password": "notsosecret",
     "gameid":  "896660",
     "steamcmd":  "C:\\Steamcmd\\steamcmd.exe",
-    "forceinstalldir":  "C:\\Valheim"
+    "forceinstalldir":  "C:\\Valheim",
+    "BackupsEnabled":  "False",
+    "BackupsFolder":  "C:\\ValheimBackup",
+    "BackupsDaysToKeep":  "7"
 }
 #>
 Function Start-Valheim {
     #Starts the Valheim Server
+    #Should add -saves parameter to allow config file to specify where save files are located.
     $Process = get-process valheim_server -ErrorAction SilentlyContinue
     if ($Process){
         write-host "Valheim already running"
@@ -34,19 +38,12 @@ Function Stop-Valheim {
     #Sends Ctrl+C to the Valheim window, which saves the server first and shuts down cleanly
     $Process = get-process valheim_server -ErrorAction SilentlyContinue
     if ($Process){
-        $MemberDefinition = '
-        [DllImport("kernel32.dll")]public static extern bool FreeConsole();
-        [DllImport("kernel32.dll")]public static extern bool AttachConsole(uint p);
-        [DllImport("kernel32.dll")]public static extern bool GenerateConsoleCtrlEvent(uint e, uint p);
-        public static void SendCtrlC(uint p) {
-            FreeConsole();
-            AttachConsole(p);
-            GenerateConsoleCtrlEvent(0, p);
-            FreeConsole();
-            AttachConsole(uint.MaxValue);
-        }'
-        Add-Type -Name 'dummyName' -Namespace 'dummyNamespace' -MemberDefinition $MemberDefinition
-        [dummyNamespace.dummyName]::SendCtrlC($Process.ID)
+        # be sure to set $ProcessID properly. Sending CTRL_C_EVENT signal can disrupt or terminate a process
+        $ProcessID = $Process.Id
+        $encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("Add-Type -Names 'w' -Name 'k' -M '[DllImport(""kernel32.dll"")]public static extern bool FreeConsole();[DllImport(""kernel32.dll"")]public static extern bool AttachConsole(uint p);[DllImport(""kernel32.dll"")]public static extern bool SetConsoleCtrlHandler(uint h, bool a);[DllImport(""kernel32.dll"")]public static extern bool GenerateConsoleCtrlEvent(uint e, uint p);public static void SendCtrlC(uint p){FreeConsole();AttachConsole(p);GenerateConsoleCtrlEvent(0, 0);}';[w.k]::SendCtrlC($ProcessID)"))
+        start-process powershell.exe -argument "-nologo -noprofile -executionpolicy bypass -EncodedCommand $encodedCommand"
+        write-host "Waiting for Process $($ProcessID) to stop"
+        Wait-Process -id $ProcessID
     } else {
         write-host "no process found, not terminating anything"
     }
@@ -61,6 +58,43 @@ Function Get-ValheimLatestVersion {
     $BuildID = $json.data.$($config.gameid).depots.branches.public.buildid
     Return $BuildID
 }
+
+
+Function Start-ValheimBackupRegular {
+    #This will back up Valheim world
+    #Should implement support for -saves parameter in config file and link the backup there if specified.
+    
+    #Check if backup folder exists. If not, create it.
+    if ($Config.BackupsFolder){
+        if (!(test-path $Config.Backupsfolder)){New-Item $Config.Backupsfolder -ItemType Directory}
+    
+        $DBFile = Get-ChildItem "$($env:userprofile)\appdata\LocalLow\IronGate\Valheim\worlds\\$($Config.world).db"
+        $FWLFile = Get-ChildItem "$($env:userprofile)\appdata\LocalLow\IronGate\Valheim\worlds\$($Config.world).fwl"
+        $Date = get-date $DBFile.LastWriteTime -format "yyyy-MM-dd_HH-mm"
+        $Destination = "$($Config.Backupsfolder)\$($config.world)\$date"
+        $Destination
+        if (!(test-path $Destination)){New-Item -Path $Destination -ItemType Directory
+            Copy-Item $DBFile -Destination $Destination
+            Copy-Item $FWLFile -Destination $Destination
+        }
+    } Else {
+        Write-Host "Update config file with BackupsFolder"
+    }
+}
+Function Start-ValheimBackupCleanup {
+    #This will clean up old backups
+    
+    if ($Config.BackupsDaysToKeep){
+        #Check if backup folder exists. If not, create it.
+        if (!(test-path $Config.Backupsfolder)){New-Item $Config.Backupsfolder -ItemType Directory}
+        $DeleteOlderThan = (Get-Date).AddDays(-$($Config.BackupsDaysToKeep))
+        $FolderToClean = "$($Config.Backupsfolder)\$($config.world)"
+        Get-ChildItem $FolderToClean | Where-Object {$_.LastWriteTime -lt $DeleteOlderThan} | Remove-Item -Recurse
+    } Else {
+        Write-Host "Update config file with BackupDaysToKeep"
+    }
+}
+
 #Run every 300 seconds forever
 $stop = "$false"
 do{
@@ -69,14 +103,18 @@ do{
     
     if ($BuildID -ne $CurrentBuildID){
         #New version detected. Initiating patching
+        write-host "New version found. Stopping and updating Valheim_server"
         Stop-Valheim
         Update-Valheim
     } else {
         Write-host "Newest buildid is current: $($BuildID)"
+    }
+    if ($Config.BackupsEnabled) {
+        Start-ValheimBackupRegular
+        Start-ValheimBackupCleanup
     }
     #This will start Valheim after patching, and even if it's not patched but crashed for some reason
     Start-Valheim
     #Will run every 5 minutes (300 seconds)
     Start-Sleep -Seconds 300
 } while ($stop)
-
